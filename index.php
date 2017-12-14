576ba13ad8d714e7fa4a2c1785affe01b5ec95f2
Antoine3007 <antoineduclos1994@gmail.com>
2017-12-14 14:16:45 +0100

    add connexion to database

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-	echo 'Hello moijv !';
+	require_once 'connexion.php';
+
+	$queryGame = $connexion->prepare('SELECT * FROM game');
+
+	$queryGame->execute();
+
+	$game = $queryGame->fetchAll(PDO::FETCH_ASSOC);
+
+	echo '<pre>'; print_r($game); echo '</pre>';
